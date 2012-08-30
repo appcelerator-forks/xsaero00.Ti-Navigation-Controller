29f54a14a476e3ca6e3a9461ba45ffd3dd93dc3f
Mike Starov <mike.starov@gmail.com>
2012-08-30 10:27:32 -0700

    CHG: Event driven version of navigation controller.

--- Resources/NavigationController.js
+++ Resources/NavigationController.js
@@ -1,26 +1,49 @@
-NavigationController = function() {
+exports.NavigationController = function() {
 	this.windowStack = [];
 };
 
-NavigationController.prototype.open = function(/*Ti.UI.Window*/windowToOpen) {
+exports.NavigationController.prototype.open = function(/*Ti.UI.Window*/windowToOpen) {
+	Ti.API.log("Open function.");
 	//add the window to the stack of windows managed by the controller
 	this.windowStack.push(windowToOpen);
 
 	//grab a copy of the current nav controller for use in the callback
 	var that = this;
 	windowToOpen.addEventListener('close', function() {
-		if (that.windowStack.length > 1)	// don't pop the last Window, which is the base one
-		{
-			that.windowStack.pop();
+		Ti.API.log("Event 'close': " + this.title);
+		that.windowStack.pop();
+		
+		// close dependent window ?
+		if (this.toClose) {
+			 Ti.API.log("Invoke close on dependent window:" + this.toClose.title);
+			 // close "parent" window, do not use animation (it looks wierd with animation)
+			 (that.navGroup) ? that.navGroup.close(this.toClose, {animated : false}) : this.toClose.close();
 		}
+		// open dependent window ?
+		if (this.toOpen) {
+			Ti.API.log("Invoke open on dependent window:" + this.toOpen.title);
+			 that.open(this.toOpen);
+		} 
+		
+		Ti.API.log("End event 'close'. Stack: " + that.windowStack.map(function(v) {return v.title}));
 	});
 	
+	windowToOpen.addEventListener('set.to.close', function(dict) {
+		Ti.API.log("Event 'set.to.close': " + this.title);
+		this.toClose = dict.win;
+	});
+	
+	windowToOpen.addEventListener('set.to.open', function(dict) {
+		Ti.API.log("Event 'set.to.open': " + this.title);
+		this.toOpen = dict.win;
+	});
+
 	//hack - setting this property ensures the window is "heavyweight" (associated with an Android activity)
 	windowToOpen.navBarHidden = windowToOpen.navBarHidden || false;
 
 	//This is the first window
-	if(this.windowStack.length === 1) {
-		if(Ti.Platform.osname === 'android') {
+	if (this.windowStack.length === 1) {
+		if (Ti.Platform.osname === 'android') {
 			windowToOpen.exitOnClose = true;
 			windowToOpen.open();
 		} else {
@@ -34,44 +57,39 @@ NavigationController.prototype.open = function(/*Ti.UI.Window*/windowToOpen) {
 	}
 	//All subsequent windows
 	else {
-		if(Ti.Platform.osname === 'android') {
+		if (Ti.Platform.osname === 'android') {
 			windowToOpen.open();
 		} else {
 			this.navGroup.open(windowToOpen);
 		}
 	}
+	Ti.API.log("End Open. Stack: " + this.windowStack.map(function(v) {return v.title}));
 };
 
 //go back to the initial window of the NavigationController
-NavigationController.prototype.home = function() {
-	//store a copy of all the current windows on the stack
-	var windows = this.windowStack.concat([]);
-	for(var i = 1, l = windows.length; i < l; i++) {
-		(this.navGroup) ? this.navGroup.close(windows[i]) : windows[i].close();
+exports.NavigationController.prototype.home = function() {
+	Ti.API.log("Home function.");
+	if (this.windowStack.length > 1) {
+		// setup chain reaction
+		for (var i = this.windowStack.length - 1; i > 1; i--)
+			// set dependent window
+			this.windowStack[i].fireEvent('set.to.close', {win: this.windowStack[i - 1]});
+        
+        // start chain reaction, close first window
+		(this.navGroup) ? this.navGroup.close(this.windowStack[this.windowStack.length - 1]) : this.windowStack[this.windowStack.length - 1].close();
 	}
-	this.windowStack = [this.windowStack[0]]; //reset stack
+	Ti.API.log("End Home. Stack: " + this.windowStack.map(function(v) {return v.title}));
 };
 
-/*
- * This back function came from Matthew Lanham's version on
- * https://github.com/swanify/Titanium-Navigation-Controller
- */
-NavigationController.prototype.back = function(w) {
-		//store a copy of all the current windows on the stack
-		if(Ti.Platform.osname === 'android') {
-			w.close();
-		} else {
-			this.navGroup.close(w);
-		}
+exports.NavigationController.prototype.openFromHome = function(windowToOpen) {
+	Ti.API.log("openFromHome function.");
+	if(this.windowStack.length == 1)
+		this.open(windowToOpen);
+	else
+	{
+		// delegate opening of the window to last window to close
+		this.windowStack[1].fireEvent('set.to.open', {win: windowToOpen});
+		this.home();
+	}
+	Ti.API.log("End openFromHome. Stack: " + this.windowStack.map(function(v) {return v.title}));
 };
-
-module.exports = NavigationController;
-
-/*
- * Author: www.sunfishempire.com
- * This is my version of the NavigationController file
- * Modified from Kevin Whinnery's version on 
- * https://github.com/kwhinnery/NavigationController
- * Uses the commonJS pattern, works on Android 2.3.3, iPhone 5.1, and 
- * Titanium Mobile 2.0 SDK.
- */
