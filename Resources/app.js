9d8b2bbd1b0e6f32de8e87bfe267a9fba4f7c6f3
Mike Starov <mike.starov@gmail.com>
2012-08-30 10:32:11 -0700

    CHG: Corrected require statements.

--- Resources/app.js
+++ Resources/app.js
@@ -1,5 +1,6 @@
-var NavigationController = require('NavigationController'),
-TestWindow = require('TestWindow');
+//require the UI components necessary to drive the test
+var NavigationController = require('NavigationController').NavigationController,
+	TestWindow = require('TestWindow').TestWindow;
 
 //create NavigationController which will drive our simple application
 var controller = new NavigationController();
